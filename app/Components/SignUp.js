5b8fef1df7431a7eb8c225515dbf476b53720a7f
Humpero <noah.grebliauskas@gmail.com>
2018-03-09 00:41:46 +0200

    Fix pointers in pull request

--- app/Components/SignUp.js
+++ app/Components/SignUp.js
@@ -1,17 +1,66 @@
-$(function () {
-    $('#signUp-form').on('submit', function (e) {
-        e.preventDefault();
+const OfficeData = [
+  {
+    text: 'Kaunas',
+    value: 'kaunas',
+  },
+  {
+    text: 'Vilnius',
+    value: 'vilnius',
+  },
+  {
+    text: 'Chicago',
+    value: 'shicago',
+  },
+  {
+    text: 'toronto',
+    value: 'toronto',
+  },
+  {
+    text: 'London',
+    value: 'london',
+  },
+];
 
-        var result = {
-            firstName: e.target.firstName.value,
-            lastName: e.target.lastName.value,
-            email: e.target.email.value,
-            office: e.target.office.value,
-            slackName: e.target.slackName.value,
-            password: e.target.password.value,
-            repeatPassword: e.target.repeatPassword.value,
-        }
+// Function to populate the office select options
+(function () {
+  const officeSelect = document.getElementById('office');
 
-        console.log(result);
-    });
-});
+  OfficeData.forEach(element => {
+    const opt = document.createElement('option');
+    opt.value = element.value;
+    opt.innerHTML = element.text;
+
+    officeSelect.appendChild(opt);
+  });
+})();
+
+// Function to catch the submit action from the signUp-form element
+(function () {
+  const form = document.getElementById('signUp-form');
+
+  if (form.attachEvent) {
+    form.attachEvent('submit', processSignUpForm);
+  } else {
+    form.addEventListener('submit', processSignUpForm);
+  }
+})();
+
+// Function to process the signUp-form input and print it to console
+function processSignUpForm(e) {
+  if (e.preventDefault) e.preventDefault();
+
+  const result = {
+    firstName: e.target.firstName.value,
+    lastName: e.target.lastName.value,
+    email: e.target.email.value,
+    office: e.target.office.value,
+    slackName: e.target.slackName.value,
+    password: e.target.password.value,
+    repeatPassword: e.target.repeatPassword.value,
+  };
+
+  console.log(result);
+
+  // Return false to prevent the default form behavior
+  return false;
+}
