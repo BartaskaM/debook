c2930346c3e348126fdbf37e705659efb8d08859
Humpero <noah.grebliauskas@gmail.com>
2018-03-09 01:45:23 +0200

    Implement form validation, reorder form inputs

--- app/Components/SignUp.js
+++ app/Components/SignUp.js
@@ -49,18 +49,48 @@ const OfficeData = [
 function processSignUpForm(e) {
   if (e.preventDefault) e.preventDefault();
 
-  const result = {
-    firstName: e.target.firstName.value,
-    lastName: e.target.lastName.value,
-    email: e.target.email.value,
-    office: e.target.office.value,
-    slackName: e.target.slackName.value,
-    password: e.target.password.value,
-    repeatPassword: e.target.repeatPassword.value,
-  };
+  if (emailValidate() && repeatPasswordValidate()) {
+    const result = {
+      firstName: e.target.firstName.value,
+      lastName: e.target.lastName.value,
+      email: e.target.email.value,
+      office: e.target.office.value,
+      slackName: e.target.slackName.value,
+      password: e.target.password.value,
+      repeatPassword: e.target.repeatPassword.value,
+    };
 
-  console.log(result);
+    console.log(result);
+  }
+  else
+    alert('Please fix form errors');
 
   // Return false to prevent the default form behavior
   return false;
+}
+
+// Email validation function
+function emailValidate() {
+  if (document.getElementById('signUp-form').email.value === 'email@email.com') {
+    document.getElementById('emailHelp').innerHTML = 'Email already in use';
+  }
+  else {
+    document.getElementById('emailHelp').innerHTML = '';
+
+    return true;
+  }
+}
+
+// Password validation function
+function repeatPasswordValidate() {
+  const signUpForm = document.getElementById('signUp-form');
+
+  if (signUpForm.password.value !== signUpForm.repeatPassword.value) {
+    document.getElementById('repeatPasswordHelp').innerHTML = 'Passwords do not match';
+  }
+  else {
+    document.getElementById('repeatPasswordHelp').innerHTML = '';
+
+    return true;
+  }
 }
