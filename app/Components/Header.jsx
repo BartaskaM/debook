aff5ebc034db95557ff5880f7933d0815a563789
Humpero <noah.grebliauskas@gmail.com>
2018-03-16 17:20:39 +0200

    Revert 2 placeholder links

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -8,8 +8,8 @@ class Header extends React.Component {
         <li><Link to='/login'>LoginTabs</Link></li>
         <li><Link to='/main'>MainTabs</Link></li>
         <li><Link to='/profile'>Profile</Link></li>
-        <li><Link to='/device/'>Device</Link></li>
-        <li><Link to='/office/'>Office</Link></li>
+        <li><Link to='/device/1'>Device id 1</Link></li>
+        <li><Link to='/office/1'>Office id 1</Link></li>
       </ul>
     );
   }
