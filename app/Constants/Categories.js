ca45931aa2706dc14dc7c2bd4699d7ee12c9ec03
Humpero <noah.grebliauskas@gmail.com>
2018-03-17 23:04:11 +0200

    Clean up code

--- app/Constants/Categories.js
+++ app/Constants/Categories.js
@@ -1,9 +1,24 @@
 const Categories = [
-  'Placeholder category 1',
-  'Placeholder category 2',
-  'Placeholder category 3',
-  'Placeholder category 4',
-  'Placeholder category 5',
+  {
+    id: 0,
+    title:'Placeholder category 1',
+  },
+  {
+    id: 1,
+    title:'Placeholder category 2',
+  },
+  {
+    id: 2,
+    title:'Placeholder category 3',
+  },
+  {
+    id: 3,
+    title:'Placeholder category 4',
+  },
+  {
+    id: 4,
+    title:'Placeholder category 5',
+  },
 ];
 
 export default Categories;
