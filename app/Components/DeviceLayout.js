02ce93fb9f0b0325e3385e615ed362cb21ffbff9
Andrius Kondratjev <andriusk1997@gmail.com>
2018-03-18 15:56:18 +0200

    - Adding data to js arrays - Adding functionality so it would work with multiple devices (all devices have a button and label to communicate) - fixing css - removing errors

--- app/Components/DeviceLayout.js
+++ app/Components/DeviceLayout.js
@@ -1,13 +1,60 @@
-var checkmark = document.getElementById("checkmark");
-var button = document.getElementById("booking");
+const checkmark = document.getElementsByClassName("checkmark");
+const button = document.getElementsByClassName("booking");
 
+const DeviceData = [{
+    image:'https://images-eu.ssl-images-amazon.com/images/I/612rXM6eMSL._SX385_.jpg',
+    name: 'Apple iPad Pro 10.5", 256GB',
+    identification_num: '0000000497',
+    os: 'macOS High Sierra(version 10.13)',
+    location: 'Kaunas',
+  },{
+    image:'https://static.acer.com/up/Resource/Acer/Notebooks/AGW2%20Aspire%20V5/Photo%20Gallery/20130912/Aspire-V5-photogallery-01.png',
+    name: 'Acer Aspire V5-573G 15.6 inches',
+    identification_num: '0000000498',
+    os: 'Windows',
+    location: 'Kaunas',
+  }, {
+    image:'https://images-eu.ssl-images-amazon.com/images/I/612rXM6eMSL._SX385_.jpg',
+    name: 'Apple iPad Pro 10.5", 256GB',
+    identification_num: '0000000497',
+    os: 'macOS High Sierra(version 10.13)',
+    location: 'Kaunas',
+  },{
+    image:'https://static.acer.com/up/Resource/Acer/Notebooks/AGW2%20Aspire%20V5/Photo%20Gallery/20130912/Aspire-V5-photogallery-01.png',
+    name: 'Acer Aspire V5-573G 15.6 inches',
+    identification_num: '0000000498',
+    os: 'Windows',
+    location: 'Kaunas',
+  },
+];
 
-button.onclick = function() {
-    checkmark.innerHTML = "Unavailable";
-    checkmark.style.color="red";
-    checkmark.style.backgroundColor = "#FF7F50";
-    button.style.backgroundColor = "#E9E9E9";
-    
-    
-    button.disabled=true;
+function formHTML(){
+    const htmlString = DeviceData.map( x => {
+      return '<div class="oneDevice"><img class="tablet" src='+x.image+'><label class="checkmark">Available</label><h2>'
+      +x.name+'</h2><li>Identification number: '+x.identification_num+'</li><li>OS: '+x.os+'</li><li>Location: '
+      +x.location+'</li><div><button class="reserve" type="button">Reserve</button> <button class="booking" type="button">Book Device</button></div></div>'
+    }).join('');
+  
+    return htmlString;
+}
+
+function renderHTML(){
+  document.querySelector('.item-list').innerHTML=formHTML();
+}
+document.onload=renderHTML();
+
+$('.booking').click(function(){
+  var buttonIndex = $(this).closest('.booking').index('.booking');
+  console.log(buttonIndex);
+  changeLabel(buttonIndex)
+
+});
+
+function changeLabel(index) {
+    checkmark[index].innerHTML = "Unavailable";
+    checkmark[index].style.color="red";
+    checkmark[index].style.backgroundColor = "#FF7F50";
+    button[index].style.backgroundColor = "#E9E9E9";
+    console.log("works");
+    button[index].disabled=true;
 }
