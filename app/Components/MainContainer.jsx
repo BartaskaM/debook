384f02ed337864e1f75c82d6647b58bbad639856
Marius Kimbirauskas <wariuz@gmail.com>
2018-03-25 01:44:52 +0200

    Removed temporary import

--- app/Components/MainContainer.jsx
+++ app/Components/MainContainer.jsx
@@ -8,7 +8,6 @@ import Profile from './Profile';
 import DeviceDetails from './DeviceDetails';
 import OfficeDetails from './OfficeDetails';
 import User from '../Constants/User';
-import LogIn from './LoginTabs/LogIn';
 
 class MainContainer extends React.Component {
   render() {
