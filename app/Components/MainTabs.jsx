57bb000835fd4c7650f71d33739d440f538d133e
Matas <m.bartaska@gmail.com>
2018-03-27 22:23:48 +0300

    Move to absolute paths.

--- app/Components/MainTabs.jsx
+++ app/Components/MainTabs.jsx
@@ -3,9 +3,9 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import DeviceList from './DeviceList';
-import Devices from '../Constants/Devices';
-import User from '../Constants/User';
-import * as devicesActions from '../ActionCreators/devicesActions';
+import Devices from 'Constants/Devices';
+import User from 'Constants/User';
+import * as devicesActions from 'ActionCreators/devicesActions';
 import Filters from 'components/Filters';
 
 
