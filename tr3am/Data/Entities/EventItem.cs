d38220a4d52963e8a297aa693d4d7847de73d8af
Marius Kimbirauskas <wariuz@gmail.com>
2018-04-12 23:38:51 +0300

    Initial commit

---
+++ tr3am/Data/Entities/EventItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tr3am.Data.Entities
+{
+    public class EventItem
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public int Device { get; set; }
+        public int Office { get; set; }
+        public int User { get; set; }
+        public DateTime Date_time { get; set; }
+    }
+}
---
+++ tr3am/DataContracts/IEventsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tr3am.Data.Entities;
+using tr3am.DataContracts.Requests.Offices;
+
+namespace tr3am.DataContracts
+{
+    interface IEventsRepository
+    {
+        List<EventItem> GetAll();
+        EventItem GetById(int id);
+        EventItem Create(OfficeItemRequest request);
+        void Update(int id, OfficeItemRequest request);
+        void Delete(int id);
+    }
+}
