cd5467d5a1fa05dc4e9cc0f6d38803127a99da94
Matas <m.bartaska@gmail.com>
2018-04-26 00:43:53 +0300

    Reservation table nob shows that there are no reservations instead of being hidden

--- tr3am/app/Components/ReservationsTable/index.jsx
+++ tr3am/app/Components/ReservationsTable/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import List from 'material-ui/List';
+import List, { ListItem } from 'material-ui/List';
 import Grid from 'material-ui/Grid';
+import Divider from 'material-ui/Divider';
+
 import Row from './row';
 import { withStyles } from 'material-ui/styles';
 import Styles from './Styles';
@@ -11,36 +13,41 @@ import { dateToHours } from 'Utils/dateUtils';
 class ReservationsTable extends React.Component {
   renderRows(){
     const { classes, selectedDevice, reservations, currentDate, users } = this.props;
-    return reservations.filter(res => res.device === selectedDevice && 
+    const reservationsForThisDay = reservations.filter(res => res.device === selectedDevice && 
     res.from.getDate() === currentDate.getDate() && 
     res.from.getMonth() === currentDate.getMonth() &&
-    res.from.getFullYear() === currentDate.getFullYear())
-      .sort(res => res.from)
-      .map((res, i) => {
-        const { from, to, user } = res;
-        const userInfo = users.find(usr => usr.id === user);
-        return <Row key={i} first={`${dateToHours(from)} - ${dateToHours(to)}`} 
-          second={`${userInfo.firstName} ${userInfo.lastName}`} 
-          styleClass={classes.row}
-          addDivider={true}/>;
-      }
-      );
+    res.from.getFullYear() === currentDate.getFullYear());
+    return reservationsForThisDay.length == 0 ? 
+      <Grid item xs={12}>
+        <ListItem>
+          <Grid container>
+            <Grid item xs={12}><Divider /></Grid>
+            <Grid item xs={12}>No reservations today.</Grid>
+          </Grid>
+        </ListItem>
+      </Grid> :
+      reservationsForThisDay
+        .sort(res => res.from)
+        .map((res, i) => {
+          const { from, to, user } = res;
+          const userInfo = users.find(usr => usr.id === user);
+          return <Row key={i} first={`${dateToHours(from)} - ${dateToHours(to)}`} 
+            second={`${userInfo.firstName} ${userInfo.lastName}`} 
+            styleClass={classes.row}
+            addDivider={true}/>;
+        }
+        );
   }
 
   render(){
     const { 
       classes, 
-      reservations, 
-      selectedDevice, 
-      currentDate, 
     } = this.props;
     return (
       <Grid container spacing={16}>
         <Grid item xs={12}>
           <List className={classes.officeList}>
-            {reservations.filter( res => res.device === selectedDevice &&
-            res.from.getDate() === currentDate.getDate()).length > 0 && 
-            <Row first="TIME" second="RESERVED BY" styleClass={classes.topRow}/>}
+            <Row first="TIME" second="RESERVED BY" styleClass={classes.topRow}/>
             {this.renderRows()}
           </List>
         </Grid>
